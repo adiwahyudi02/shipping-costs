0c31e0b932c0d1bdd09f9938fde49eee43e56063
Adi Wahyudi <adi.whyd02@gmail.com>
2024-12-05 10:59:38 +0700

    feat: add couriersOptions constant

---
+++ constants/courier.ts
@@ -0,0 +1,14 @@
+export const couriersOptions = [
+  {
+    value: "jne",
+    label: "JNE",
+  },
+  {
+    value: "pos",
+    label: "POS",
+  },
+  {
+    value: "tiki",
+    label: "TIKI",
+  },
+];
