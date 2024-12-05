ba5a582387fe31996839a29329c60e0555936e0e
Adi Wahyudi <adi.whyd02@gmail.com>
2024-12-05 10:24:54 +0700

    feat: setup global sass utilities that includes variables, mixins, and functions

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `
+      @use '@/styles/utilities' as *
+    `,
+  },
 };
 
 export default nextConfig;
